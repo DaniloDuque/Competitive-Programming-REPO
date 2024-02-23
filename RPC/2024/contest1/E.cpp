92ff0599a475e170fd1fb600a5fdba7aac19a3ed
Danilo <105374584+DaniloDuque@users.noreply.github.com>
2024-02-22 22:57:24 -0600

    Add files via upload

---
+++ RPC/2024/contest1/E.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long double x, y;
+
+
+int main(){
+
+    scanf("%Lf%Lf", &x, &y);
+    long double z = x/y;
+    z *= (100-y)/(100-x);
+    printf("%.15Lf\n", z); 
+    return 0;
+
+}
